3fab538e14f7db4ba6e7b563a7182099089d89b2
nicole diallo <nicole@nicoles-MacBook-Air.local>
2019-04-04 09:10:38 -0400

    NIKI DIALLO

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 let canvas = document.getElementById('main');
 let c = canvas.getContext('2d');
 
-function init() {
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
+let pos = new Vector(60, 60);
+let vel = new Vector(.5, .2);
+
+function init() {
+  pos.print();
+    // console.log(vec.toString());
     update();
 }
 
 function update() {
+  c.clearRect(0, 0, canvas.width, canvas.height);
+
+  pos.addVector(vel);
+
+  c.arc(pos.x, pos.y, 50, 0, Math.PI*2, false);
+  c.closePath();
+  c.stroke();
 
     requestAnimationFrame(update);
 }
--- vector.js
+++ vector.js
@@ -1,7 +1,22 @@
 class Vector {
-    
-    constructor() {
-
+    // constructor Vector(x: any, y: any): Vector
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
     }
+
+    addVector(pos) {
+      this.x += pos.x;
+      this.y += pos.y;
+      return this;
+    }
+
+    toString() {
+      return '<' + this.x + ', ' + this.y + '>';
+    }
+
+print() {
+  console.log(this.toString());
+}
 
 }
